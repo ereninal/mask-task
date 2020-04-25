b5dccf91d3c1ac7094930fcd507bb542f86d776f
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-25 22:33:19 +0300

    Entities layer created.

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -9,4 +9,11 @@ namespace Workaround
             Console.WriteLine("Hello World!");
         }
     }
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
 }
